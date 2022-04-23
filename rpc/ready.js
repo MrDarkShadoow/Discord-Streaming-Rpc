c57e3dfd486d593283eb53942d6d8466f71ab1e6
Shadoow159258 <98183554+MrDarkShadoow@users.noreply.github.com>
2022-04-23 15:50:52 -0300

    Create ready.js

---
+++ rpc/ready.js
@@ -0,0 +1,65 @@
+///////////////////////////////////
+
+// Youtube:
+//https://www.youtube.com/channel/UC4zkPH1ticImcO4-fjM2pZA
+
+///////////////////////////////////
+
+const rpc = require("discordrpcgenerator");
+
+
+
+
+let NAME = "Name"
+let DETAILS = "Details"
+let STATE = "State"
+let URL = ""
+let APPID = "921069363971383406"
+let LARGEIMG = "Id Img"
+let LARGETEXT = "Large Text"
+let SMALLIMG = "Id Img"
+let SMALLTEXT = "Id Img"
+
+
+
+module.exports = async (client) => {
+  const small = await rpc.getRpcImage
+  (APPID, SMALLIMG);//1°setApplicationId, 2°Name(Key)smallimage
+  
+  
+rpc.getRpcImage
+(APPID, LARGEIGM)//1°setApplicationId, 2°Name(Key)largename
+.then(large => {
+  
+  
+     let presence = new rpc.Rpc()
+               
+         .setName(NAME)
+         .setDetails(DETAILS)
+         .setState(STATE)
+        
+        
+         .setUrl(URL)
+         .setType("STREAMING")
+         .setApplicationId(APPID)
+        
+         .setAssetsLargeImage(large.id)
+         .setAssetsLargeText(LARGETEXT)
+        
+         .setAssetsSmallImage(small.id)
+         .setAssetsSmallText(SMALLTEXT)
+        
+
+          
+           client.user.setStatus("dnd");
+      
+     console.log(presence.toDiscord())
+        
+      
+      client.user.setPresence(presence.toDiscord()).catch(console.error);
+      
+    })
+      
+console.log("Rpc ready")
+
+}
